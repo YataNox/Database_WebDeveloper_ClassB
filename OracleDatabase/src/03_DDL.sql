52038a1f01dff49e46aae5ef0713ce2fbc7d0854
YataNox <rladnwls0523@naver.com>
2021-09-30 19:42:30 +0900

    Update 03_DDL.sql

---
+++ OracleDatabase/src/03_DDL.sql
@@ -0,0 +1,16 @@
+-- DDL(Database Definition Language) - 데이터 정의어
+
+-- 테이블의 생성(Create)
+
+-- Create TABLE 테이블 이름(
+-- 		필드명 1 DATATYPE [DEFAULT값 OR 제약조건 및 형식],
+-- 		필드명 2 DATATYPE [DEFAULT값 OR 제약조건 및 형식],
+-- 		필드명 3 DATATYPE [DEFAULT값 OR 제약조건 및 형식],
+--		...
+-- );
+
+-- Create Table 명령의 세부 규칙
+-- 테이블의 이름은 객체를 의미할 수 있는 적절한 이름을 사용합니다.(자바 변수 이름 규칙과 거의 동일)
+-- 다른 테이블과 중복되지 않게 테이블 이름을 지정합니다.
+-- 한 테이블 내에서 필드 이름도 중복되지 않게 합니다.
+-- 각 필드들은 ","(컴마)로 구분하여 생성합니다.
