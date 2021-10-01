ed812012c088b1e8d36c0146a90be72c0bc3f5fd
kim <rladnwls0523@naver.com>
2021-10-01 20:14:21 +0900

    Update 06_DDL_View_Sequence.sql

--- OracleDatabase/src/06_DDL_View_Sequence.sql
+++ OracleDatabase/src/06_DDL_View_Sequence.sql
@@ -14,4 +14,48 @@
 -- 구성하는 형식입니다. - 원본의 데이터 변화가 실시간으로 반영됩니다.
 -- 생성 방법
 -- Create or Replace View 뷰이름 as (select 구문)
+-- select 명령 : 실제 테이블의 부분 집합(행 일부 또는 전부, 열 일부 또는 전부)
 -- select 명령과 join 명령 학습 후 다시 상세하게 공부할 예정입니다.
+
+
+-- * 오라클 - 시퀀스(Sequence)
+-- 			: 테이블 내의 유일한 숫자를 자동으로 생성하는 자동 번호 발생기.
+-- 			: 테이블 생성 후 시퀀스(일련번호)를 따로 만들어야 한다.
+-- 생성 방법
+-- create sequence 시퀀스 이름 start with 시작 숫자 increment by 증가량;
+
+-- 주로 number 형식에 기본키 값으로 사용합니다.
+-- 일련번호 정도로 이해하셔도 무방합니다.
+-- number(자리수) : 자료형의 자리수가 몇자리냐에 따라 그만큼 숫자가 증가 할 수 있습니다.
+
+select * from memberlist;
+
+-- [1] 시퀀스의 생성
+create sequence book_seq start with 1 increment by 1;
+
+-- [2] 현재 시퀀스가 어디까지 증가되여져 있는지 확인.
+select book_seq.currval, book_seq.nextVal from dual;
+
+-- [3] 시퀀스 수정 : 최대 증가값을 14까지로 제한.
+alter sequence book_seq maxvalue 14;
+
+-- [4] 시퀀스 삭제
+drop sequence book_seq;
+
+-- [5] 시퀀스 재생성 : 다음 숫자부터 시작하게 하여 기존 레코드와 중복 되지 않게 합니다.
+create sequecne book_seq start with 15 increment by 1;
+
+-- * 현재 로그인한 사용자가 만들거나 보유하고 있는 테이블의 목록을 조회
+select * from tabs;
+
+-- 1부터 1씩 늘어나느 member_seq rent_seq를 생성해주세요.
+create sequence member_seq start with 1 increment by 1;
+create sequence rent_seq start with 1 increment by 1;
+
+insert into memberlist(membernum, name, phone) values(book_seq.nextVal, '홍길동','010-1111-2222');
+insert into memberlist(membernum, name, phone) values(book_seq.nextVal, '홍길서','010-3333-4444');
+insert into memberlist(membernum, name, phone) values(book_seq.nextVal, '홍길남','010-5555-6666');
+insert into memberlist(membernum, name, phone) values(book_seq.nextVal, '홍길북','010-7777-8888');
+
+
+
