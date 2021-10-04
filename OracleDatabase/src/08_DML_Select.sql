4ece1732e788edd06045f0168e28946d99e344e6
YataNox <rladnwls0523@naver.com>
2021-10-04 21:03:46 +0900

    Update 08_DML_Select.sql

--- OracleDatabase/src/08_DML_Select.sql
+++ OracleDatabase/src/08_DML_Select.sql
@@ -64,3 +64,50 @@ select name, phone from memberlist where Bpoint is not null;
 
 -- [16] 도서 제목에 두 번째 글자가 '것'인 도서 정보
 select * from booklist where subject like '_것%';
+
+-- 성별이 null인 것을 모두 M으로 수정해주세요
+-- update memberlist set gender = 'M' where gender is null;
+
+-- emp 테이블에서 deptno가 10, 20, 30, 40 중 하나인 데이터
+select * from emp where deptno = 10 or deptno = 20 or deptno = 30 or deptno = 40;
+
+-- 조건식(ANY, SOME, ALL, (IN))
+-- where절에서 사용하는 그룹 내 해당 요소 찾기 함수들
+-- 1. ANY
+select * from emp where deptno = any(10,20,30);
+-- ANY() 괄호안에 나열된 내용 중 어느 하나라도 해당하는 것이 있따면 검색 대상으로 함
+
+-- 2. --SOME 조건식 - ANY와 동일 - IN과도 동일
+SELECT * FROM emp WHERE deptno = some(10,20,40);
+
+-- 3. ALL
+select empno, sal from emp where sal = all(2000, 3000, 4000);
+select empno, sal from emp where sal <> all(2000, 3000, 4000);
+-- 괄호안의 모든 값이 동시 만족해야하는 조건이므로 해당하는 레코드가 없을 때가 대부분입니다.
+-- 두 번째 사용 예처럼 모두와 다를 때를 위해 사용되곤 합니다.
+-- 사용빈도 수도 낮습니다.
+
+-- 4. 논리 조건식 NOT
+select empno, sal from emp where deptno not in(10,20,30,40);
+-- in()안에 있는 것과 하나도 매칭되지 않은 값이 검색대상
+
+
+-- 정렬(Sort) - where 구문 뒤에, 또는 구문의 맨 끝에 Order by 필드명 [desc]
+-- select 명령의 결과를 특정 필드 값의 오름차순이나 내림차순으로 정렬하라는 명령
+-- asc : 오름차순 정렬, 쓰지 않으면 기본 오름차순 정렬로 실행됩니다.
+-- desc : 내림차순 정렬, 내림차순 정렬을 위해서는 반드시 필드명 뒤에 써야하는 키워드 입니다.
+
+-- emp 테이블에서
+-- sal이 1000 이상인 데이터를 ename의 오름차순으로 정렬하여 조회
+select * from emp where sal>=1000 order by ename; -- 오름차순 asc는 생략
+-- sal이 1000 이상인 데이터를 ename의 내림차순으로 정렬하여 조회
+select * from emp where sal>=1000 order by ename desc;
+-- job으로 내림차순 정렬 
+select * from emp emp order by job desc;
+-- job으로 내림차순 정렬 후 같은 job_id 사이에서는 순서를 hiredate의 내림차순으로 정렬
+select * from emp emp order by job desc, hiredate desc;
+-- 두 가지 이상의 정렬 기준이 필요하다면 위와 같이 (,)으로 구분해서 두 가지 기준을 지정해주며,
+-- 위의 예제로 봤을 때 job으로 1차 내림차순 정렬하고, 같은 job 값들 사이에 hiredate로
+-- 내림차순 정렬합니다.
+
+
