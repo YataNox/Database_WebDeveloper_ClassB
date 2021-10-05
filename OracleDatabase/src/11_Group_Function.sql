7fe69ddcf341e8183a6daee4ac5f248294ce45fe
kim <rladnwls0523@naver.com>
2021-10-05 21:20:41 +0900

    Update 11_Group_Function.sql

--- OracleDatabase/src/11_Group_Function.sql
+++ OracleDatabase/src/11_Group_Function.sql
@@ -1,3 +1,10 @@
 -- sum() : 특정 필드의 합계
 select sum(rentprice) as "대여가격 합계" from booklist;
+select sum(rentprice) as "대여가격 합계" from booklist where inprice >= 18000;
 
+-- count() : 필드내의 데이터 갯수(레코드 갯수)
+select count(*) as "회원 인원수" from memberlist;
+select count(*) as "회원 인원수" from MEMBERLIST where bpoint >= 2000;
+
+-- avg() : 평균
+select avg(inprice) from booklist;
