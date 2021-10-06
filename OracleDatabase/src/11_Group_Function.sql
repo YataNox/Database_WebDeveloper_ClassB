cb2d60c08dc5e2dac0700739d63b98f6e40e3620
YataNox <rladnwls0523@naver.com>
2021-10-06 16:31:57 +0900

    Update 11_Group_Function.sql

--- OracleDatabase/src/11_Group_Function.sql
+++ OracleDatabase/src/11_Group_Function.sql
@@ -15,5 +15,28 @@ select MAX(inprice) from booklist;
 -- MIN : 최소 값
 select MIN(inprice) from booklist;
 
+-- variance(분산) stddev(표준편차)
+select variance(salary), stddev(salary) from EMPLOYEES;
+select salary from EMPLOYEES;
+
+
+-- group by : 하나의 필드를 지목해서 같은 값끼리 그룹을 형성한 결과를 도출합니다.
+select * from RENTLIST;
+-- 도서별 대여 건수
+select booknum, count(*) from rentlist group by booknum;
+-- 날짜별 할인 금액의 평균
+select rentdate, avg(discount) from rentlist group by rentdate;
+-- rentlist 날짜별 대여 건수
+select rentdate, count(*) from rentlist group by rentdate;
+-- employees 테이블의 부서 id별 급여의 평균
+select * from EMPLOYEES;
+select department_id, avg(salary) from employees group by DEPARTMENT_id;
+-- kor_loan_status 테이블의 period(년도와 월)을 1차 그룹으로 region(지역)을 2차 그룹으로한
+-- 대출 잔액(loan_jan_amt)의 합계
+select * from kor_loan_status;
+select period, region, sum(loan_jan_amt) from KOR_LOAN_STATUS
+group by period, region;
+
+
 
 
