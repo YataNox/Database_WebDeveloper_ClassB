5f9a68213309394c428f7d12cca21e18e623ce68
kim <rladnwls0523@naver.com>
2021-10-07 20:57:39 +0900

    Create JDBC_Update.java

---
+++ OracleDatabase/src/JDBC02/JDBC_Update.java
@@ -0,0 +1,11 @@
+package JDBC02;
+
+public class JDBC_Update 
+{
+	public static void main(String[] args) 
+	{
+		// 첫 번째로 수정할 도서의 도서번호 입력
+		// 두 번째로 수정할 도서의 항목( 출판년도, 입고가격, 대여가격, 등급)
+		// 네 가지 중 하나를 입력받고 해당 내용을 수정하는 코드를 작성하세요
+	}
+}
