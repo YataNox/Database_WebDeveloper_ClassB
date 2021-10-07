9fc736eca2ee8bd61ca1332eade9479369c5ac18
kim <rladnwls0523@naver.com>
2021-10-07 18:13:57 +0900

    Create JDBC_Update.java

--- OracleDatabase/src/JDBC01/JDBC_Update.java
+++ OracleDatabase/src/JDBC01/JDBC_Update.java
@@ -30,6 +30,31 @@ public class JDBC_Update
 			System.out.print("수정할 항복을 선택하세요. 1. 이메일 2. 전화번호");
 			String input = sc.nextLine();
 			
+			String sql = "";
+			switch(input) {
+				case "1":
+					System.out.print("수정할 이메일을 입력하세요 : ");
+					String email = sc.nextLine();
+					sql = "Update customer set email = ? where num = ?";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, email);
+					pstmt.setInt(2, Integer.parseInt(num));
+					break;
+				case "2":
+					System.out.print("수정할 전화번호를 입력하세요 : ");
+					String tel = sc.nextLine();
+					sql = "Update customer set tel = ? where num = ?";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, tel);
+					pstmt.setInt(2, Integer.parseInt(num));
+					break;
+			}
+			int result = pstmt.executeUpdate();
+			if(result == 1)
+				System.out.println("수정 성공!");
+			else
+				System.out.println("수정 실패!");
+			
 		}
 		catch(ClassNotFoundException e) {
 			e.printStackTrace();
