67092a62566470c1983451d8e0025e24aed6f0f3
kim <rladnwls0523@naver.com>
2021-10-07 16:16:30 +0900

    Update JDBC_Connect.java

--- OracleDatabase/src/JDBC01/JDBC_Connect.java
+++ OracleDatabase/src/JDBC01/JDBC_Connect.java
@@ -20,8 +20,10 @@ public class JDBC_Connect
 		try 
 		{
 			Class.forName(driver);
-			DriverManager.getConnection(url, id, pw);
+			// 연결 드라이버 매니저가 데이터베이스 연결을 하고, 연결해준 연결 인스턴스를 con 변수에 저장
+			con = DriverManager.getConnection(url, id, pw);
 			System.out.println("데이터베이스 연결에 성공했습니다.");
+			// 데이터 베이스 작업
 		}
 		catch(ClassNotFoundException e) 
 		{
