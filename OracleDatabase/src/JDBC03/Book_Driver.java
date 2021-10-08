75d3f75443eef61d13aaa56acb57810ea38da8f9
YataNox <rladnwls0523@naver.com>
2021-10-08 16:50:35 +0900

    Update Book_Driver.java

--- OracleDatabase/src/JDBC03/Book_Driver.java
+++ OracleDatabase/src/JDBC03/Book_Driver.java
@@ -1,9 +1,39 @@
 package JDBC03;
 
+import java.util.Scanner;
+
 public class Book_Driver
 {
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("\n*** 메뉴 선택 ***");
+			System.out.printf("1. 데이터추가.");
+			System.out.printf("2. 데이터열람.");
+			System.out.printf("3. 데이터수정.");
+			System.out.printf("4. 데이터삭제.");
+			System.out.println("5. 프로그램 종료.");
+			System.out.print(">> 메뉴선택 : ");
+			String choice = sc.nextLine();
+			
+			if(choice.equals("5"))
+				break;
+			
+			switch(choice) {
+				case "1": 
+					break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				default:
+					System.out.println("잘못 입력된 값입니다.");
+			}
+		}
+		System.out.println("프로그램 종료");
 		
 	}
 }
