125de171f4bbfb084e9a2e086d7bf35e33289a39
YataNox <rladnwls0523@naver.com>
2021-10-08 16:52:14 +0900

    Update Book_Dao.java

--- OracleDatabase/src/JDBC03/Book_Dao.java
+++ OracleDatabase/src/JDBC03/Book_Dao.java
@@ -1,10 +1,23 @@
 package JDBC03;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 // Database Access Object
 public class Book_Dao
 {
 	public static void main(String[] args)
 	{
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String id = "scott";
+		String pw = "tiger";
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
 		
 	}
 }
