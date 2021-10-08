a89e4c86e621d918ac062958621eb8c702d734a9
YataNox <rladnwls0523@naver.com>
2021-10-08 17:21:10 +0900

    Create Book_Dto.java

---
+++ OracleDatabase/src/JDBC03/Book_Dto.java
@@ -0,0 +1,12 @@
+package JDBC03;
+
+// Dto : Data Transfer Object 다른 말로 java bean
+public class Book_Dto 
+{
+	private int booknum;
+	private String subject;
+	private int makeyear;
+	private int inprice;
+	private int rentprice;
+	private String grade;
+}
