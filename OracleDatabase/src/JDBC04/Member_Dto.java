cb3bd4b58c5e4f6b5dcb815af5ef0e236d8d94f5
YataNox <rladnwls0523@naver.com>
2021-10-09 22:59:24 +0900

    Update Memberlist Manager System - Write Member_Dto.java

--- OracleDatabase/src/JDBC04/Member_Dto.java
+++ OracleDatabase/src/JDBC04/Member_Dto.java
@@ -1,6 +1,72 @@
 package JDBC04;
 
+import java.util.Date;
+
 public class Member_Dto 
 {
-
+	private int membernum;
+	private String name;
+	private String phone;
+	private Date birth;
+	private Date joindate = new Date();
+	private int Bpoint;
+	private String gender;
+	private int age;
+	
+	public int getMembernum() {
+		return membernum;
+	}
+	public void setMembernum(int membernum) {
+		this.membernum = membernum;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	
+	public Date getJoindate() {
+		return joindate;
+	}
+	public void setJoindate(Date joindate) {
+		this.joindate = joindate;
+	}
+	
+	public int getBpoint() {
+		return Bpoint;
+	}
+	public void setBpoint(int bpoint) {
+		Bpoint = bpoint;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
