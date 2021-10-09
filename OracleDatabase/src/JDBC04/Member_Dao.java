67e97bf3c2cb5a33f5a3a4b887cd25848eaeea7e
YataNox <rladnwls0523@naver.com>
2021-10-09 23:24:01 +0900

    Update Memberlist Manager System - Write Dao, Driver layout

--- OracleDatabase/src/JDBC04/Member_Dao.java
+++ OracleDatabase/src/JDBC04/Member_Dao.java
@@ -1,6 +1,88 @@
 package JDBC04;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Member_Dao 
 {
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	private String id = "scott";
+	private String pw = "tiger";
 	
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	// db 연결함수
+		public Connection getConnection() 
+		{
+			Connection con = null;
+			try {
+				Class.forName(driver);
+				con = DriverManager.getConnection(url, id, pw);
+			}catch(ClassNotFoundException e) {
+				e.printStackTrace();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+			return con;
+		}
+		
+		// db 연결 해제 함수
+		public void close() 
+		{
+			try {
+				if(con != null)
+					con.close();
+				if(pstmt != null)
+					pstmt.close();
+				if(rs != null)
+					rs.close();
+			}catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		// Memberlist 전체 조회 함수------------------------------------------------------------------------
+		public ArrayList<Member_Dto> selectAll() 
+		{
+			ArrayList<Member_Dto> list = new ArrayList<Member_Dto>();
+			
+			return list;
+		} 
+		// Memberlist 튜플 삽입 함수------------------------------------------------------------------------
+		public int insertSql(Member_Dto Mdto) 
+		{
+			
+			return 0;
+		}
+		// Memberlist 튜플 삭제 함수------------------------------------------------------------------------
+		public int deleteSql(String num) 
+		{
+			int result = 0;
+			
+			return result;
+		}
+		// Memberlist 튜플 수정 함수------------------------------------------------------------------------
+		public int updateSql(Member_Dto Mdto) 
+		{
+			int result = 0;
+			
+			return result;
+		}
+		// Memberlist 전송객체 전달 함수------------------------------------------------------------------------
+		public Member_Dto getDto(String num) 
+		{
+			Member_Dto mdto = null;
+			
+			return null;
+		}
+		
 }
--- OracleDatabase/src/JDBC04/Menber_Driver.java
+++ OracleDatabase/src/JDBC04/Menber_Driver.java
@@ -1,9 +1,73 @@
 package JDBC04;
 
+import java.util.Scanner;
+
+import JDBC03.Book_Dao;
+
 public class Menber_Driver 
 {
 	public static void main(String[] args) 
 	{
+		Scanner sc = new Scanner(System.in);
+		
+		// 기능 실행 무한 반복
+		while(true) {
+			System.out.println("\n*** 메뉴 선택 ***");
+			System.out.printf("1. 데이터추가. ");
+			System.out.printf("2. 데이터열람. ");
+			System.out.printf("3. 데이터수정. ");
+			System.out.printf("4. 데이터삭제. ");
+			System.out.println("5. 프로그램 종료. ");
+			System.out.print(">> 메뉴선택 : ");
+			String choice = sc.nextLine();
+			
+			// 5 입력시 프로그램 종료
+			if(choice.equals("5"))
+				break;
+			
+			// 공통적으로 사용할 멤버 접속 객체
+			Member_Dao Mdao = new Member_Dao();
+			
+			// 입력한 번호에 따른 기능 실행
+			switch(choice) {
+				case "1": 
+					insert(sc, Mdao); // 158 Line
+					break;
+				case "2":
+					select(Mdao); // 140 Line
+					break;
+				case "3":
+					update(sc, Mdao); // 60 Line
+					break;
+				case "4":
+					delete(sc, Mdao); // 47 Line
+					break;
+				default:
+					System.out.println("잘못 입력된 값입니다.");
+			}
+		}
+		
+		System.out.println("프로그램 종료");
+		sc.close();
+	}
 
+	// memberlist 조회 함수 - Dao.selectSql 메소드와 연결
+	private static void select(Member_Dao mdao) {
+		
+	}
+	
+	// memberlist 삽입 함수 - Dao.insertSql 메소드와 연결
+	private static void insert(Scanner sc, Member_Dao mdao) {
+		
+	}
+
+	// memberlist 삭제 함수 - Dao.deleteSql 메소드와 연결
+	private static void delete(Scanner sc, Member_Dao mdao) {
+		
+	}
+	
+	// memberlist 수정 함수 - Dao.updateSql 메소드와 연결
+	private static void update(Scanner sc, Member_Dao mdao) {
+		
 	}
 }
